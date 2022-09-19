94289af2278d2e4332af28cf4179182d2d613919
Keyradin <dnkeyr@gmail.com>
2022-09-19 17:08:31 +0530

    commitrd

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+x        """Initialize a new Rectangle.
 
         Args:
             width (int): The width of the new rectangle.
